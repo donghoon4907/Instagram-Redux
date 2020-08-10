8588e5b8f4b76677e5af607eb804e2efe9a79a53
동훈 이 <dhlee@gemiso.com>
2020-08-10 19:32:34 +0900

    personal branch

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-console.log("test");
+console.log("test2");
 ReactDOM.render(<App />, document.getElementById("root"));
